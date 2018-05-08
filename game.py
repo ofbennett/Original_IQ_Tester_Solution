5e16bfef6fed0306936593007d4db745bdd981a3
ofbennett <ofbennett@gmail.com>
2018-05-08 12:41:19 +0100

    edit comment

--- game.py
+++ game.py
@@ -9,7 +9,7 @@ This code computes all possible solutions the The Original IQ Tester puzzle. In 
 See here for details of the puzzle and rules:
 https://www.amazon.com/Channel-Classic-Challenging-Handcrafted-Original/dp/B001XM3DGM
 
-There are approx 1 million possible sequences in the game when a grid of depth 5 is chosen (as is the case in the wooden puzzle itself)!
+There are approx 1 million possible game sequences when a grid of depth of 5 is chosen (grid depth = rows in the grid, the wooden game shown has a depth of 5) but this increases quickly if larger grids are chosen.
 """
 
 class Grid(object):
